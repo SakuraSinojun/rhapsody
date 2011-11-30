d3e85956b66d9251b90b3e1b331bee6fd668f029
SakuraSinojun@gmail.com <SakuraSinojun@gmail.com@8520ec38-563b-4c89-2369-92dc90947fe1>
2011-11-30 09:22:45 +0000

    CMake脚本，mingw通过。

---
+++ project/GRPGM/game/cmake/MinGW.cmake
@@ -0,0 +1,8 @@
+
+
+set(CMAKE_AR                    "i586-mingw32msvc-ar")
+set(CMAKE_CXX_COMPILER          "i586-mingw32msvc-g++")
+set(CMAKE_CXX_FLAGS             "-Wall -Werror -O2")
+set(CMAKE_CXX_FLAGS_DEBUG       "-g")
+set(CMAKE_RANLIB                "i586-mingw32msvc-ranlib")
+
